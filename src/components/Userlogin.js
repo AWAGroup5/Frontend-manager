5683a28cfd3ec86786822f95faa72a85c247b5fc
idRisto <risto.alanen@gmail.com>
2021-12-13 15:45:31 +0200

    Fixed login

--- src/components/Userlogin.js
+++ src/components/Userlogin.js
@@ -64,7 +64,7 @@ export default function Userlogin(props) {
             } catch (error) {
                 console.error(error.message);
                 setTimeout(() => {
-                    // setIsLoading(false);
+                    setIsLoading(false);
                     setError(true)
                     setUsername('')
                     setPassword('')
